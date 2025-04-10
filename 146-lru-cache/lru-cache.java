4503c6820694471441f57ca5c49506cbb67efa0b
Aditya Mohan Gupta <114954982+AdityaMohanGupta@users.noreply.github.com>
2025-04-11 00:00:02 +0530

    Time: 1010 ms (5.01%) | Memory: 128.2 MB (7.78%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,36 @@
+class LRUCache {
+    static int capacity;
+    static Queue<Integer> q;
+    static Map<Integer,Integer> map;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        q=new LinkedList<>();
+        map=new HashMap<>();
+    }
+    
+    public int get(int key) {
+        if (map.containsKey(key)){
+            q.remove(key);
+            q.offer(key);
+            return map.get(key);
+        } 
+        else 
+            return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            q.remove(key);
+            q.offer(key);
+            map.put(key,value);
+        } else {
+            if(q.size() != capacity) {
+                q.offer(key);
+            } else {
+                map.remove(q.poll());
+                q.offer(key);
+            }
+                map.put(key,value);
+        }
+    }
+}
