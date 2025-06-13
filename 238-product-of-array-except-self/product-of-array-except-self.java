9b0b6cb443bd754782c948bc29d7540fe689b62f
Aditya Mohan Gupta <114954982+AdityaMohanGupta@users.noreply.github.com>
2025-06-13 16:09:01 +0530

    Time: 3 ms (22.55%) | Memory: 55.9 MB (30.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -19,3 +19,21 @@ class Solution {
         return nums;
     }
 }
+
+// without extra space
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int[] result = new int[nums.length];
+//         Arrays.fill(result, 1);
+//         for(int i = nums.length - 2; i >= 0; i--){
+//             result[i] = result[i + 1] * nums[i + 1];
+//         }
+//         int tmp = nums[0];
+//         for(int j = 1; j < nums.length; j++){
+//             result[j] = result[j] * tmp;
+//             tmp = tmp * nums[j];
+//         }
+//         return result;
+        
+//     }
+// }
